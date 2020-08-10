9a814253b716232859923339434c58c6b285aa0b
slurio <samantha.lurio@gmail.com>
2020-08-10 11:01:36 -0400

    update

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -8,29 +8,29 @@ class Passenger
         self.class.all << self
     end
 
-    def self.all #returns all passengers in an array
+    def self.all #returns an array of all passengers
         @@all
     end
 
-    def rides #Returns an array of Ride instances that this person has been on
+    def rides #returns an array of Ride instances that this person has been on
         Ride.all.select do |ride|
             ride.passenger == self
         end
     end
 
-    def drivers #Returns an array of Driver instances that this person has rode with
+    def drivers #returns an array of Driver instances that this person has rode with
         rides.map do |ride|
             ride.driver
         end
     end
 
-    def total_distance #Returns the floating number that represents the total distance the passenger has travelled
+    def total_distance #returns the floating number that represents the total distance the passenger has travelled
         rides.map do |ride|
             ride.distance
         end.sum
     end
 
-    def self.premium_members #Returns an array of all Passengers who have travelled over 100 miles
+    def self.premium_members #returns an array of all Passengers who have travelled over 100 miles
         self.all.select do |passenger|
             passenger.total_distance > 100
         end
