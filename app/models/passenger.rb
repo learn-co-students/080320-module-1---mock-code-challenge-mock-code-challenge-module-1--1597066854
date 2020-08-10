a5fb0843cb019da1b2ee3d08b27c01893bcb7fd1
royFloresNyc <royfloresnyc@gmail.com>
2020-08-10 10:07:39 -0400

    Passenger class basic structure

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,35 @@
 
+class Passenger
+    attr_reader :name 
+    @@all = []
+
+    def initialize(name) #expects a string argument
+        @name = name
+        @@all << self
+    end 
+
+    ##class methods
+
+    def self.all
+        @@all
+    end 
+
+    def premium_members
+    
+    end 
+
+    ##instance methods
+
+    def rides
+
+    end 
+
+    def drivers
+
+    end 
+
+    def total_distance
+
+    end 
+
+end 
