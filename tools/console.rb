464e53a0cfafba5dc504a45c1109b5cf5eff749f
AJavier55 <aliciajavier5@gmail.com>
2020-08-10 10:34:53 -0400

    driver/passenger names

--- tools/console.rb
+++ tools/console.rb
@@ -1,9 +1,19 @@
-require_relative '../config/environment.rb'
+require 'pry'
+# require_relative '../config/environment.rb'
+require_relative '../app/models/driver.rb'
+require_relative '../app/models/passenger.rb'
+require_relative '../app/models/ride.rb'
 
 def reload
   load 'config/environment.rb'
 end
 
+joe = Passenger.new("Joe")
+kim = Passenger.new("Kim")
+greg = Passenger.new("Greg")
 
+emily = Driver.new("Emily")
+kevin = Driver.new("Kevin")
+randy = Driver.new("Randy")
 
 binding.pry
