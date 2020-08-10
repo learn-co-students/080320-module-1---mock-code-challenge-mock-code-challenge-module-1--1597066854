9945cd2b1b9f395a4ede695a4edaa37ee107ac23
kain <kaindoneson@gmail.com>
2020-08-10 10:19:48 -0400

    fleshed out the pasenger class

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -5,6 +5,10 @@ class Passenger
         @@all;
     end
 
+    def self.premium_members
+        self.all.filter{|p| p.total_distance > 100};
+    end
+
     attr_reader :name;
 
     def initialize(name)
@@ -12,7 +16,29 @@ class Passenger
         self.class.all << self;
     end
 
+    def rides
+        Ride.all.filter {|r| r.Passenger == self};
+    end
+
+
+    def drivers
+        self.rides.map {|r| r.driver} 
+    end
+
+    def total_distance
+        self.rides.map{|r| r.distance}.reduce(0){|sum, dist| sum + dist}
+    end
 
 end
 
 # name works thru the reader
+
+# rides filter j table looks for self as ref in Rides.all
+
+# drivers calls rides and  maps
+
+# total distance call rides and map distances plus reduco
+
+# self.all -checko
+
+# self.prem - filter Pass.all and ask if total dist > 100
