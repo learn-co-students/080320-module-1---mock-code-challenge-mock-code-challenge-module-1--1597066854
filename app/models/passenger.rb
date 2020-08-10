0a02abde187e6161b08a5d18c6f63c2550fee848
kain <kaindoneson@gmail.com>
2020-08-10 10:10:23 -0400

    rides class is fleshed out

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,18 @@
+class Passenger
+    
+    @@all = []
+    def self.all
+        @@all;
+    end
 
+    attr_reader :name;
+
+    def initialize(name)
+        @all = name;
+        self.class.all << self;
+    end
+
+
+end
+
+# name works thru the reader
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,29 @@
+#jtable
 
+class Ride
+
+    @@all = [];
+
+    def self.all
+        @@all;
+    end
+
+    attr_accessor :passenger, :driver, :distance
+
+    def initialize(passenger, driver, distance)
+        @passenger = passenger;
+        @driver = driver;
+        @distance = distance.to_f;
+
+        self.class.all.push(self);
+    end
+
+    def self.average_distance
+        (self.all.reduce(0){|sum, el| sum + el.distance}) / self.all.length;
+    end
+end
+
+# ride works thru accessor
+# driver works thru accessor
+# distance works thru accessor and uses .to_f to make float;
+# Ride.average distance uses reduce to sum the Ride.all array and the divides by the length
