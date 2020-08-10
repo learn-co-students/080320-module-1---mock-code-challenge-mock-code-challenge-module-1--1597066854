6ce2bb1af946556d976d8d7831ce37d263bba1de
jhonny689 <jhonny.chamoun@gmail.com>
2020-08-10 10:11:20 -0400

    adding the class Ride in file ride.rb

---
+++ app/models/ride.rb
@@ -0,0 +1,29 @@
+class Ride
+    # Class variables section
+    @@all = []
+
+    # Attrs definition section
+    attr_accessor :passenger, :driver, :distance
+
+    # Class constructor sectio
+    def initialize(passenger, driver, distance)
+        self.passenger = passenger
+        self.driver = driver
+        self.distance = distance
+
+        self.class.all << self
+    end
+
+    # Custom instance attribute getters/ setters section
+
+    # instance methods section
+
+    # Class methods section
+    def self.all
+        @@all
+    end
+
+    def self.average_distance
+        self.all.inject{|sum, trip| sum + trip.distance } / self.all.size
+    end
+end
