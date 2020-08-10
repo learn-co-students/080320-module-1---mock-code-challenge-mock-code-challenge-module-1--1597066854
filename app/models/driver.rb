ee69850375370721034c72d271adb667f67b9823
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-10 11:07:14 -0400

    completed

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -38,21 +38,6 @@ class Driver
 
 end
 
-# #### Driver
-
-
-
-
 # - `Driver.mileage_cap(distance)`
 #   - Takes an argument of a distance (float) and returns an array of all Drivers who have driven over the mileage
 
-
-# A Driver should be initialized with a name as a string.
-# - `Driver#name`
-#   - Returns the driver's name
-# - `Driver#rides`
-#   - Returns an array of all Rides a driver has made
-# - `Driver.all`
-#   - Returns an array of all Drivers
-# - `Driver#passenger_names`
-#   - Returns an array of all Passengers' names a driver has driven. The names should be **unique** (no repeats).
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -18,7 +18,6 @@ class Passenger
     end
   end
 
-  ##supposed to be unique?##
   def drivers
     self.rides.map do |ride|
       ride.driver
@@ -38,28 +37,3 @@ class Passenger
   end
 
 end
-
-# #### Passenger
-
-
-
-
-
-
-
-
-
-# - `Passenger#total_distance`
-#   - Returns the floating number that represents the total distance the passenger has travelled using the service
-# - `Passenger#name`
-#   - Returns the name of the passenger
-# - `Passenger#rides`
-#   - Returns an array of Ride instances that this person has been on
-# A Passenger should be initialized with a name as a string. After the Passenger has been initialized, it shouldn't be changed.
-
-# - `Passenger.all`
-#   - Returns an array of all Passengers
-# - `Passenger#drivers`
-#   - Returns an array of Driver instances that this person has rode with
-# - `Passenger.premium_members`
-#   - Returns an array of all Passengers who have travelled over 100 miles in total with the service
