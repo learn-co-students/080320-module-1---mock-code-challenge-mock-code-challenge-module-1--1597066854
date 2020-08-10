9041efe066e0219e0f75aa39e99572256717ceed
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-08-10 10:04:40 -0400

    passenger class

--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,15 @@
-require_relative '../config/environment.rb'
+require_relative "../config/environment.rb"
+require_relative "../app/models/driver.rb"
+require_relative "../app/models/passenger.rb"
+require_relative "../app/models/ride.rb"
 
 def reload
   load 'config/environment.rb'
 end
+
+tom = Passenger.new("Tom")
+larry = Passenger.new("Harry")
+sally = Passenger.new("Sally")
 
 
 # Put your variables here~!
