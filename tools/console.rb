a9d2ce34c16256bce2331d242848937f19dc0d0a
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-10 10:10:46 -0400

    created test instances in console

--- tools/console.rb
+++ tools/console.rb
@@ -8,10 +8,24 @@ end
 # Put your variables here~!
 
 ###### DRIVER ######
-d1 = 
+d1 = Driver.new("Sam")
+d2 = Driver.new("Judy")
+d3 = Driver.new("Jordan")
+d4 = Driver.new("Rachael")
 
 ###### PASSENGER ######
-###### RIDE ######
+p1 = Passenger.new("Blade")
+p2 = Passenger.new("Toni")
+p3 = Passenger.new("Juan")
+p4 = Passenger.new("Val")
 
-r1 = 
+###### RIDE ######
+r1 = Ride.new(d1, p1, 6.0)
+r2 = Ride.new(d2, p2, 12.5)
+r3 = Ride.new(d3, p3, 3.2)
+r4 = Ride.new(d4, p4, 2.8)
+r5 = Ride.new(d2, p4, 7.8)
+r6 = Ride.new(d1, p3, 3.3)
+
+
 binding.pry
