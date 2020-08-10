26d9f386854e16abd06c309642942d381698c112
D1yr <alexander.m.sahli@gmail.com>
2020-08-10 10:35:24 -0400

    exit
    git add .!

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,14 @@
+class Ride
+    attr_reader :passenger, :driver, :distance
+    @@all = []
+    def initialize(passenger, rider, distance)
+        @passenger = passenger
+        @driver = driver
+        @distance = 4.6
+        @@all << self
+    end
 
+
+    
+
+end
