fd4001288ff997bf89c169f8b5a384b93c75b2f8
anickel101 <anson.nickel@gmail.com>
2020-08-10 10:04:01 -0400

    Ride class completed

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,23 @@
+class Ride
 
+    attr_reader :driver, :passenger
+    @@all = []
+
+    def initialize(driver, passenger, distance)
+        @driver = driver
+        @passenger = passenger
+        @distance = distance
+        @@all << self
+    end
+
+    def self.all
+        @all
+    end
+
+    def self.average_distance
+        total_dist = self.all.sum {|ride| ride.distance}
+        num_rides = self.all.count
+        total_dist / num_rides
+    end
+
+end
