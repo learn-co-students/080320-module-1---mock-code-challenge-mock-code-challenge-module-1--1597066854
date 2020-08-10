b2731eb7f4c3c565a089e08c3ae136f8a88331b6
jallenmesser <jallenmessersmith@gmail.com>
2020-08-10 12:56:35 -0400

    finished

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,24 @@
+class Ride
+    attr_accessor :driver, :passenger, :distance
 
+    @@all = []
+
+    def initialize(driver, passenger, distance)
+        @driver = driver
+        @passenger = passenger
+        @distance = distance
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.average_distance
+        average = 0
+        all.each{|ride| average += (ride.distance/all.length)}
+        average 
+    end
+
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -7,4 +7,15 @@ end
 
 # Put your variables here~!
 
+jallen = Passenger.new("Jallen")
+caryn = Passenger.new("Caryn")
+
+meghann = Driver.new("Meghann")
+isabelle = Driver.new("Isabelle")
+
+ride1 = Ride.new(meghann, jallen, 1.6)
+ride2 = Ride.new(isabelle, caryn, 20.11)
+ride3 = Ride.new(meghann, caryn, 100)
+ride4 = Ride.new(meghann, caryn, 12)
+
 binding.pry
