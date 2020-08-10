cb0da2bfbdf8addb9d0615edc2bcd912fab4f972
royFloresNyc <royfloresnyc@gmail.com>
2020-08-10 10:28:36 -0400

    Complete Ride class

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,22 @@
 
+class Ride
+    attr_accessor :driver, :passenger, :distance
+
+    @@all = []
+
+    def initialize(driver, passenger, distance)
+        @driver = driver
+        @passenger = passenger
+        @distance = distance
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def self.average_distance
+        self.all.map { |ride| ride.distance }.reduce {|sum, n| sum + n } / self.all.count
+    end 
+
+end 
