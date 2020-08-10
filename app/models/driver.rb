bbef727e9db03b605c146804f69518df69242234
royFloresNyc <royfloresnyc@gmail.com>
2020-08-10 10:15:39 -0400

    Driver class basic structure

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,32 @@
 
+class Driver
+
+    attr_reader :name
+
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end 
+
+    ##class methods
+
+    def self.all
+      @@all
+    end 
+
+    def self.mileage_cap(distance)
+
+    end 
+
+    ##instance methods
+
+    def passenger_names
+    
+    end 
+
+    def rides
+
+    end 
+
+end 
