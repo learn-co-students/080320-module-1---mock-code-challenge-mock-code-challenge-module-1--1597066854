661e1bba6e3d2b05ec135ae68e83fa060a1b6553
TeddyBradsher <teddybradsher@gmail.com>
2020-08-10 10:26:30 -0400

    so far so good

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,22 @@
+class Passenger
+    attr_reader :name
+    @@all = []
 
+    def initialize (name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def rides
+        Ride.all.select do |trip|
+            trip.passenger == self
+        end 
+    end
+
+
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -7,4 +7,16 @@ end
 
 # Put your variables here~!
 
+
+driver1 = Driver.new("ted")
+driver2 = Driver.new("rick")
+driver3 = Driver.new("mike")
+
+pass1 = Passenger.new("joe")
+pass2 = Passenger.new("angel")
+pass3 = Passenger.new("chris")
+
+ride1 = Ride.new(driver1, pass1, 1.5)
+ride2 = Ride.new(driver2, pass2, 2.5)
+
 binding.pry
