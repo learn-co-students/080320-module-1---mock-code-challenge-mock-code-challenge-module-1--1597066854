7fb458e02462b5b50372b6a4bae5666c2f02402f
kain <kaindoneson@gmail.com>
2020-08-10 10:27:06 -0400

    wrote blue print for driver in comments goint to create the methods now

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,23 @@
+class Driver
+    
+    @@all = [];
+    def self.all
+        @@all;
+    end
 
+    attr_accessor :name;
+    def initialize(name)
+        @name = name;
+
+        self.class.all.push(self);
+    end
+
+
+end
+
+# name works thru accessor
+# rides ask rides for self ref with filter
+# passenger_names call self.rides and then map to r.passenger.names
+# works thru class var/method self.all
+# Driver.milage - make function distance that returns drivers total distance by maping and reducibng from
+# self.rides...then u can just filter thru Driver.all by askings if d.distance > arg
