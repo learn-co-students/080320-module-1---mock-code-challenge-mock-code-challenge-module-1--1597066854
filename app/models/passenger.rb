becfa57045a8255153deeadd4e90d1296999b23a
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-10 10:37:38 -0400

    created drivers instance method for Passenger class

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -17,21 +17,35 @@ class Passenger
       ride.passenger == self
     end
   end
+
+  ##supposed to be unique?##
+  def drivers
+    self.rides.map do |ride|
+      ride.driver
+    end.uniq
+  end
+
 end
 
 # #### Passenger
-# A Passenger should be initialized with a name as a string. After the Passenger has been initialized, it shouldn't be changed.
 
-# - `Passenger#rides`
-#   - Returns an array of Ride instances that this person has been on
+
+
 # - `Passenger#drivers`
 #   - Returns an array of Driver instances that this person has rode with
 # - `Passenger#total_distance`
 #   - Returns the floating number that represents the total distance the passenger has travelled using the service
-# - `Passenger.all`
-#   - Returns an array of all Passengers
+
 # - `Passenger.premium_members`
 #   - Returns an array of all Passengers who have travelled over 100 miles in total with the service
 
+
+
 # - `Passenger#name`
 #   - Returns the name of the passenger
+# - `Passenger#rides`
+#   - Returns an array of Ride instances that this person has been on
+# A Passenger should be initialized with a name as a string. After the Passenger has been initialized, it shouldn't be changed.
+
+# - `Passenger.all`
+#   - Returns an array of all Passengers
