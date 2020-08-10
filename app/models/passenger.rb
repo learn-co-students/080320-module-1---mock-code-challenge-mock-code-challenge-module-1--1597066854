9ba2eda46786af2d4416ce6885a9e78586486051
soroosh-gb <soroosh.gb@gmail.com>
2020-08-10 10:35:56 -0400

    30min

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,33 @@
 
+class Passenger
+    attr_reader :name
+    @@all = []
+
+    def initialize (name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def name(name)
+        @name
+    end
+
+    def rides
+        Ride.all.filter {|ride| ride.passenger == self}
+    end
+
+    def drivers
+        self.rides.map do |ride|
+            ride.drivers
+        end
+    end
+
+    def total_distance
+        
+
+
+end
