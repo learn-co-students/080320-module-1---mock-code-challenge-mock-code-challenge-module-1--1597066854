3a4a69a240603e683ccc1a1495c9c7f66f3f3bed
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-10 10:29:04 -0400

    created instance method passengers_names for Driver class

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,7 +3,7 @@ class Driver
 
   @@all = []
 
-  def initiazlie(name)
+  def initialize(name)
     @name = name
     @@all << self
   end
@@ -12,17 +12,36 @@ class Driver
     @@all
   end
 
+  def rides
+    Ride.all.select do |ride|
+      ride.driver == self
+    end
+  end
+
+  def passengers_names
+    self.rides.map do |ride|
+      ride.passenger.name
+    end.uniq
+  end
+
+
+
 end
 
 # #### Driver
+
+# - `Driver#passenger_names`
+#   - Returns an array of all Passengers' names a driver has driven. The names should be **unique** (no repeats).
+
+
+# - `Driver.mileage_cap(distance)`
+#   - Takes an argument of a distance (float) and returns an array of all Drivers who have driven over the mileage
+
+
 # A Driver should be initialized with a name as a string.
 # - `Driver#name`
 #   - Returns the driver's name
-# - `Driver#passenger_names`
-#   - Returns an array of all Passengers' names a driver has driven. The names should be **unique** (no repeats).
 # - `Driver#rides`
 #   - Returns an array of all Rides a driver has made
 # - `Driver.all`
 #   - Returns an array of all Drivers
-# - `Driver.mileage_cap(distance)`
-#   - Takes an argument of a distance (float) and returns an array of all Drivers who have driven over the mileage
