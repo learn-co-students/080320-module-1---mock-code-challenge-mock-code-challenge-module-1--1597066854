b0806aef4b99f74b276961d28605481d5c6052a8
zhaoerik <jediofthenyy@gmail.com>
2020-08-10 10:48:05 -0400

    passenger

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -10,11 +10,43 @@ class Passenger
     end
 
     def rides
-
+        Ride.all.filter do |ride_info|
+            if ride_info.passenger == self
+                ride_info
+            end
+        end
     end
 
     def drivers
+        Ride.all.filter do |ride_info|
+            if ride_info.passenger == self
+                ride_info.driver
+            end
+        end
+    end
 
+    def total_distance
+        distance_array = []
+        Ride.all.filter do |ride_info|
+            if ride_info.passenger == self
+                ride_info.distance << distance_array
+            end
+        end
+        distance_array.inject(0){|sum,x| sum + x }
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.premium_members
+        premium_array = []
+        Ride.all.filter do |ride_info|
+            if ride_info.distance > 100
+                premium_array << ride_info.passenger
+            end
+        end
+        premium_array
     end
 
 
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -5,7 +5,7 @@ class Ride
     def initialize(driver, passenger, distance)
         @driver = driver
         @passenger = passenger
-        @distance = distance
+        @distance = 0.0
     end
     
 end
